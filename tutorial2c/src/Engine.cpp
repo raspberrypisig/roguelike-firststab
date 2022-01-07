430d1b4de38954f38cdf31f05f44788843aeeb31
Raspberry Pi <raspberrypisig@gmail.com>
2022-01-07 14:35:29 +1100

    tutorial2c: move on!

--- tutorial2c/src/Engine.cpp
+++ tutorial2c/src/Engine.cpp
@@ -7,8 +7,7 @@ constexpr auto player_foreground_color =
     TCOD_ColorRGBA{.r = 128, .g = 255, .b = 0, .a = 255};
 constexpr auto player_background_color =
     TCOD_ColorRGBA{.r = 10, .g = 10, .b = 10, .a = 255};
-constexpr auto wall_icon = tcod::CHARMAP_CP437[65];
-constexpr auto floor_icon_unicode = tcod::CHARMAP_CP437[43];
+constexpr auto floor_icon = tcod::CHARMAP_CP437[40];
 
 constexpr auto LIGHT_BLUE = tcod::ColorRGB{95, 205, 228};
 constexpr auto WHITE = tcod::ColorRGB{255, 255, 255};
@@ -20,7 +19,9 @@ Engine::Engine(int width, int height, const std::string &title,
   context = Context(console, title, font_path);
 
   offscreenConsole = tcod::Console(std::move(tcod::load_xp(path).at(0)));
-  //auto boo = offscreenConsole.at({13, 5}).ch;
+  //auto char_at_offset = offscreenConsole.at({36, 8}).ch;
+  //auto foreground_color_at_offset = offscreenConsole.at({36, 8}).fg;
+  //auto background_color_at_offset = offscreenConsole.at({36, 8}).bg;
 
   for (auto &tile : offscreenConsole) {
     if (tile.ch == 0x20) {
@@ -67,7 +68,7 @@ void Engine::handle_input() {
 
           break;
       }
-      if (console.in_bounds({new_player_pos.x, new_player_pos.y}) && offscreenConsole.at({new_player_pos.x, new_player_pos.y}).ch != wall_icon)
+      if (console.in_bounds({new_player_pos.x, new_player_pos.y}) && offscreenConsole.at({new_player_pos.x, new_player_pos.y}).ch == floor_icon)
         player.pos = new_player_pos;
       break;
   }
